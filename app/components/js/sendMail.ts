5ef0f8ad22f744666959e79da6d3bf16856b6422
byscript <henryuchenna515@gmail.com>
2023-11-01 08:58:51 +0100

    stv

--- app/components/js/sendMail.ts
+++ app/components/js/sendMail.ts
@@ -9,8 +9,9 @@ export const sendMassMail = async (
   }
 ) => {
   const date = new Date().getFullYear();
-const logo=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/amber%2Flogo.svg?alt=media&token=77edefa2-07a6-4ee0-a85f-3ec129c1e77f&_gl=1*wyjx3e*_ga*MTUxMjMyNTM2Ni4xNjkyMTQ0MzYx*_ga_CW55HF8NVT*MTY5ODgxODYwNC43LjEuMTY5ODgxODcyMy41NC4wLjA.`
-const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/amber%2Fhappyman.avif?alt=media&token=7b03effe-d6e1-49af-a10f-e41f73b3eef6&_gl=1*uj71bp*_ga*MTUxMjMyNTM2Ni4xNjkyMTQ0MzYx*_ga_CW55HF8NVT*MTY5ODgxODYwNC43LjEuMTY5ODgxODY0OC4xNi4wLjA.`
+const logo=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Amber%2Fcorporate%2Famber.png?alt=media&token=3e1d8220-971a-4ee5-a947-f926fc7eccef&_gl=1*78trt8*_ga*MTUxMjMyNTM2Ni4xNjkyMTQ0MzYx*_ga_CW55HF8NVT*MTY5ODgyMzEwMC44LjEuMTY5ODgyNTI4NC4zNy4wLjA.`
+const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Amber%2Fcorporate%2Fman.jpg?alt=media&token=d08cb464-0434-4196-9ed6-2c89baa50312&_gl=1*1suvgy2*_ga*MTUxMjMyNTM2Ni4xNjkyMTQ0MzYx*_ga_CW55HF8NVT*MTY5ODgyMzEwMC44LjEuMTY5ODgyNTI2Ni41NS4wLjA.`
+
   try {
     const { emails, showImage } = details;
 
@@ -24,20 +25,18 @@ const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.co
         messageVersions: emails.map((mail) => {
           const message = `<div style="background-color:##d4def4;color:#000012; padding: 0px 5%;font-family: Open Sans, sans-serif ">
 
-          <div style="width: 100%; box-sizing: border-box;text-align:center; background-color:#000000;   padding:12px;">
+          <div style="width: 100%; box-sizing: border-box;text-align:center;   padding:12px;">
              <img
                src="${logo}"
                alt="ambertrade.pro"
-              style="width: 150px; height:75px;"/>
-             
-          
+              style="width: 150px; height:50px; object-fit:contain"/>
               </div>
              ${
                showImage
                  ? ` <div style="width: 100%; box-sizing: border-box;">
                <img
                src="${image}"
-              style="width: 100%; height:250px; object-fit:cover; object-position:top"/>
+              style="width: 100%; height:250px; object-fit:cover; object-position:center"/>
               </div>`
                  : ""
              }
