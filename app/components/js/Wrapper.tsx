e3ff8ffceca690c307c28e3e6b2fa2e66b5dd310
henry <henryuchenna515@gmail.com>
2024-11-28 09:51:58 +0100

    fircha

--- app/components/js/Wrapper.tsx
+++ app/components/js/Wrapper.tsx
@@ -105,8 +105,18 @@ export default function Wrap({ children }: Props) {
         type="text/javascript"
         id={"zsiqchat"}
         src="//code.tidio.co/sleu2w98fkken1tlwjkj6f3tszonn7ch.js"
-      /> 
-    
+      />
+
+      <Script type="text/javascript" id="2dafd564753">
+        {`var _smartsupp = _smartsupp || {};
+_smartsupp.key = '3ae22dafd564753d5d75d500c9dbfc5a00a491d6';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);`}
+      </Script>
     </div>
   );
 }
